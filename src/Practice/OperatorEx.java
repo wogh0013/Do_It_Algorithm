8156e62830a226c02846c70b3725e8d3afeade34
username <wogh0013@naver.com>
2022-06-14 19:27:41 +0900

    commit

---
+++ src/Practice/OperatorEx.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class OperatorEx {
+    public static void main(String[] args) {
+        char lowerCase = 'a';
+        char upperCase = (char)(lowerCase - 32);
+        System.out.println(upperCase);
+    }
+}
