a32660848db71d29a12efbe309f4ed67e458fb75
username <wogh0013@naver.com>
2022-06-19 00:08:27 +0900

    commit

---
+++ src/Chap01/Unit2/TriangleLB.java
@@ -0,0 +1,22 @@
+package Chap01.Unit2;
+
+import java.util.Scanner;
+
+public class TriangleLB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        do{
+            System.out.print("변의 길이를 입력하세요. ");
+            n = sc.nextInt();
+        } while(n<=0);
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
