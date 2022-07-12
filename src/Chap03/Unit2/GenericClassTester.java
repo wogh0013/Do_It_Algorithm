86e878b9251f5155939d2c87eaa78b96a4df6b6c
username <wogh0013@naver.com>
2022-07-12 19:26:09 +0900

    commit

--- src/Chap03/Unit2/GenericClassTester.java
+++ src/Chap03/Unit2/GenericClassTester.java
@@ -15,10 +15,10 @@ public class GenericClassTester {
 
     public static void main(String[] args) {
         //다음과 같이 파라미터에 String을 넘길 수도 있고, Integer를 넘길 수도 있다.
-        GenericClass<String> s = new GenericClass<String>("ABC");
-        GenericClass<Integer> n = new GenericClass<Integer>(99);
+        GenericClass<String> s1 = new GenericClass<String>("ABC");
+        GenericClass<Integer> n1 = new GenericClass<Integer>(99);
 
-        System.out.println(s.getXyz());
-        System.out.println(n.getXyz());
+        System.out.println(s1.getXyz());
+        System.out.println(n1.getXyz());
     }
 }
